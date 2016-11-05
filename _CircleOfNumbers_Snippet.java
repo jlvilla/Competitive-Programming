63f778eb7274b6eee5bfcaf4a18a5b31d6059cf8
Jayvee Villa <jayvee.villa39@gmail.com>
2016-11-05 07:29:20 -0800

    Update _CircleOfNumbers_Snippet.java
    Correct code

--- _CircleOfNumbers_Snippet.java
+++ _CircleOfNumbers_Snippet.java
@@ -2,7 +2,12 @@ int circleOfNumbers(int n, int firstNumber) {
     if (n/2 > firstNumber)
         return n/2+firstNumber;
     else if (firstNumber > n/2)
-        return n-firstNumber-1;
+        if (n/2+firstNumber > n)
+            return (n/2+firstNumber)-n;
+        else if (n/2+firstNumber >9)
+            return (n/2+firstNumber)%10;
+        else
+            return n/2+firstNumber;
     else
         return 0;
 }
